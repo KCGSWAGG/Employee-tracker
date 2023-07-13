97c5d08bd8c88f8e490bdf6b9f2666d2784ea67b
KCGSWAGG <Tyrealworley999@yahoo.com>
2023-07-12 22:18:13 -0400

    Started working on funcationality

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS office_db;
+CREATE DATABASE office_db;
+
+USE office_db;
+
+CREATE TABLE departments (
+    id INT NOT NULL,
+    department_name TEXT NOT NULL
+)
+
+CREATE TABLE roles (
+    
+)
---
+++ server.js
@@ -0,0 +1,21 @@
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      message: 'What would you like to do?',
+      name: 'options',
+      choices: ['view all departments', 
+      'view all roles', 
+      'view all employees', 
+      'add a department', 
+      'add a role', 
+      'add an employee', 
+      'update an employee role'],
+    },
+ 
+  ])
+  .then((response) =>
+    response.confirm === response.password
+  );
