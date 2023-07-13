5af0ec443804ac55843174bf3cb0afde4336457e
KCGSWAGG <Tyrealworley999@yahoo.com>
2023-07-13 08:57:27 -0400

    started working on funcationality

--- db/schema.sql
+++ db/schema.sql
@@ -4,16 +4,29 @@ CREATE DATABASE office_db;
 USE office_db;
 
 CREATE TABLE departments (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name TEXT NOT NULL
-)
+);
+
+CREATE TABLE roles (
+     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR (30) NOT NULL,
+    department_id INT NOT NULL,
+    salary DECIMAL NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id) ON DELETE CASCADE
+);
 
 CREATE TABLE employees (
-    id INT NOT NULL,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    job_title TEXT NOT NULL,
-    departments TEXT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    job_title VARCHAR (30) NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id) ON DELETE CASCADE,
     salaries INT NOT NULL,
-    manager TEXT NOT NULL
-)
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id) ON DELETE SET NULL
+);
--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql2'); 
+const fs = require('fs');
+require('console.table');
+require('dotenv').config();
+const connection = mysql.createConnection ({
+  host: 'localhost',
+  user: 'root',
+  password: process.env.DB_PASSWORD,
+  database: 'office_db'
+ });
+ connection.connect(function(err){
+  console.log(err)
+  
+ })
 
-inquirer
+function init (){
+  inquirer
   .prompt([
     {
       type: 'list',
@@ -12,13 +27,57 @@ inquirer
       'add a department', 
       'add a role', 
       'add an employee', 
-      'update an employee role'],
+      'update an employee role']
     },
-    {
-      type ''
-    }
  
   ])
-  .then((response) =>
-    response.confirm === response.choices
-  );
+  .then((response) => {
+    if (response.options === 'view all departments'){
+      viewAllDepartments()
+    }else  if (response.options === 'view all roles'){
+    }else  if (response.options === 'view all employees'){
+
+    }else  if (response.options === 'add a department'){
+      addDepartment()
+    }else  if (response.options === 'add a role'){
+      addRole()
+    }
+    else  if (response.options === 'add an employee'){}
+    else  if (response.options === 'update an employee role'){}
+  });
+}
+
+async function viewAllDepartments (){
+    const [employees] = await connection.promise().query('select * from departments')
+    console.table(employees)
+    init()
+}
+
+async function addDepartment (){
+  const response = await inquirer.prompt([{
+    type: 'input',
+    message: 'What department would you like to add',
+    name: 'department'
+  }])
+  await connection.promise().query(`insert into departments (department_name) values ('${response.department}')`)
+  viewAllDepartments()
+  init()
+}
+
+async function addRole (){
+  const [departments] = await connection.promise().query('select * from departments')
+  const response = await inquirer.prompt([{
+    type: 'list',
+    message: 'What department would you like to add',
+    name: 'department',
+    choices: departments.map(({id,department_name})=>({
+      value: id,
+      name: department_name
+    })) 
+  }])
+  // await connection.promise().query(`insert into departments (department_name) values ('${response.department}')`)
+  viewAllDepartments()
+  init()
+}
+
+init()
