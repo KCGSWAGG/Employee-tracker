1aa0768c07a0149d0c563861160e586b871f6601
KCGSWAGG <Tyrealworley999@yahoo.com>
2023-07-13 22:00:10 -0400

    completed code so it functions right

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,23 @@
-INSERT INTO departments (id, department_name)
-VALUES (1, 'software engineer'),
-        (2, 'lawyer'),
-        (3, 'lead engineer'),
-        (4, 'sales lead'),
-        (5, 'account manager'),
-        (6, 'salesperson');
+INSERT INTO departments (department_name)
+VALUES ('software engineer'),
+        ('lawyer'),
+        ('lead engineer'),
+        ('sales lead'),
+        ('account manager'),
+        ('salesperson');
 
-INSERT INTO roles (id, title, department_id, salary)
-VALUES (1, 'software engineer', 'engineering', 120000),
-        (2, 'lawyer', 'legal', 150000),
-        (3, 'lead engineer', 'engineering', 150000)
-        (4, 'sales lead', 'sales', 100000),
-        (5, 'account manager', 'finance', 167000),
-        (6, 'salesperson', 'sales', 8000)
+INSERT INTO roles (title, department_id, salary)
+VALUES ('software engineer', 1, 120000),
+        ('lawyer', 2, 150000),
+        ('lead engineer', 3, 150000),
+        ('sales lead', 4, 100000),
+        ('account manager', 5, 167000),
+        ('salesperson', 6, 8000);
 
-INSERT INTO employees (id, first_name, last_name, job_title, role_id, salaries)
-VALUES (1, 'Tyreal', 'Worley', 'lead engineer', 3, 150000)
-        (2, 'Kobe', 'Bryant', 'account manager', 5, 167000)
-        (3, 'J', 'Cole', 'software engineer', 1, 120000)
-        (4, 'Steph', 'Curry', 'sales lead', 4, 100000)
-        (5, 'Allen', 'Iverson', 'lawyer', 2, 150000)
-        (6, 'Tank', 'Davis', 'salesperson', 6, 80000)
+INSERT INTO employees (first_name, last_name, job_title, role_id, salaries)
+VALUES ('Tyreal', 'Worley', 'lead engineer', 1, 150000),
+        ('Kobe', 'Bryant', 'account manager', 2, 167000),
+        ('J', 'Cole', 'software engineer', 3, 120000),
+        ('Steph', 'Curry', 'sales lead', 4, 100000),
+        ('Allen', 'Iverson', 'lawyer', 5, 150000),
+        ('Tank', 'Davis', 'salesperson', 6, 80000);
--- server.js
+++ server.js
@@ -1,83 +1,154 @@
 const inquirer = require('inquirer');
-const mysql = require('mysql2'); 
+const mysql = require('mysql2');
 const fs = require('fs');
 require('console.table');
 require('dotenv').config();
-const connection = mysql.createConnection ({
+
+const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: process.env.DB_PASSWORD,
   database: 'office_db'
- });
- connection.connect(function(err){
-  console.log(err)
-  
- })
+});
 
-function init (){
+connection.connect(function (err) {
+  if (err) {
+    console.error('Error connecting to the database: ' + err.stack);
+    return;
+  }
+  console.log('Connected to the database as ID ' + connection.threadId);
+});
+
+function init() {
   inquirer
-  .prompt([
-    {
-      type: 'list',
-      message: 'What would you like to do?',
-      name: 'options',
-      choices: ['view all departments', 
-      'view all roles', 
-      'view all employees', 
-      'add a department', 
-      'add a role', 
-      'add an employee', 
-      'update an employee role']
-    },
- 
-  ])
-  .then((response) => {
-    if (response.options === 'view all departments'){
-      viewAllDepartments()
-    }else  if (response.options === 'view all roles'){
-    }else  if (response.options === 'view all employees'){
-
-    }else  if (response.options === 'add a department'){
-      addDepartment()
-    }else  if (response.options === 'add a role'){
-      addRole()
-    }
-    else  if (response.options === 'add an employee'){}
-    else  if (response.options === 'update an employee role'){}
-  });
+    .prompt([
+      {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'options',
+        choices: [
+          'view all departments',
+          'view all roles',
+          'view all employees',
+          'add a department',
+          'add a role',
+          'add an employee',
+          'update an employee role'
+        ]
+      }
+    ])
+    .then((response) => {
+      if (response.options === 'view all departments') {
+        viewAllDepartments();
+      } else if (response.options === 'view all roles') {
+        viewAllRoles();
+      } else if (response.options === 'view all employees') {
+        viewAllEmployees();
+      } else if (response.options === 'add a department') {
+        addDepartment();
+      } else if (response.options === 'add a role') {
+        addRole();
+      } else if (response.options === 'add an employee') {
+        addEmployee();
+      } else if (response.options === 'update an employee role') {
+        updateEmployeeRole();
+      }
+    });
 }
 
-async function viewAllDepartments (){
-    const [employees] = await connection.promise().query('select * from departments')
-    console.table(employees)
-    init()
+async function viewAllDepartments() {
+  try {
+    const [departments] = await connection.promise().query('SELECT * FROM departments');
+    console.table(departments);
+    init();
+  } catch (error) {
+    console.error('Error retrieving departments from the database: ' + error);
+    init();
+  }
 }
 
-async function addDepartment (){
-  const response = await inquirer.prompt([{
-    type: 'input',
-    message: 'What department would you like to add',
-    name: 'department'
-  }])
-  await connection.promise().query(`insert into departments (department_name) values ('${response.department}')`)
-  viewAllDepartments()
-  init()
+async function viewAllRoles() {
+  try {
+    const [roles] = await connection.promise().query('SELECT * FROM roles');
+    console.table(roles);
+    init();
+  } catch (error) {
+    console.error('Error retrieving roles from the database: ' + error);
+    init();
+  }
 }
 
-async function addRole (){
-  const [departments] = await connection.promise().query('select * from departments')
-  const response = await inquirer.prompt([{
-    type: 'list',
-    message: 'What department would you like to add',
-    name: 'department',
-    choices: departments.map(({id,department_name})=>({
-      value: id,
-      name: department_name
-    })) 
-  }])
-  // await connection.promise().query(`insert into departments (department_name) values ('${response.department}')`)
-  viewAllDepartments()
-  init()
+async function viewAllEmployees() {
+  try {
+    const [employees] = await connection.promise().query('SELECT * FROM employees');
+    console.table(employees);
+    init();
+  } catch (error) {
+    console.error('Error retrieving employees from the database: ' + error);
+    init();
+  }
 }
 
-init()
+async function addDepartment() {
+  try {
+    const response = await inquirer.prompt([
+      {
+        type: 'input',
+        message: 'What department would you like to add?',
+        name: 'department'
+      }
+    ]);
+
+    await connection
+      .promise()
+      .query(`INSERT INTO departments (department_name) VALUES ('${response.department}')`);
+
+    console.log('Department added successfully!');
+    viewAllDepartments();
+  } catch (error) {
+    console.error('Error adding department to the database: ' + error);
+    init();
+  }
+}
+
+async function addRole() {
+  try {
+    const [departments] = await connection.promise().query('SELECT * FROM departments');
+
+    const response = await inquirer.prompt([
+      {
+        type: 'input',
+        message: 'What role would you like to add?',
+        name: 'role'
+      },
+      {
+        type: 'input',
+        message: 'What is the salary for this role?',
+        name: 'salary'
+      },
+      {
+        type: 'list',
+        message: 'Which department does this role belong to?',
+        name: 'departmentId',
+        choices: departments.map(({ id, department_name }) => ({
+          value: id,
+          name: department_name
+        }))
+      }
+    ]);
+
+    await connection.promise().query(
+      `INSERT INTO roles (title, salary, department_id) VALUES ('${response.role}', ${response.salary}, ${response.departmentId})`
+    );
+
+    console.log('Role added successfully!');
+    viewAllRoles();
+  } catch (error) {
+    console.error('Error adding role to the database: ' + error);
+    init();
+  }
+}
+
+// Implement the remaining functions: addEmployee and updateEmployeeRole
+
+init();
